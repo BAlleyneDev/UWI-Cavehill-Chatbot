e906050359f97b314cc67fb4e95fa83fa55221a8
Brandon Alleyne <b.alleyne.sa@gmail.com>
2017-09-23 15:39:06 -0400

    Inital configuration

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: 'EAAU3xvP6aC4BAM2WBI76ks01EPuuNkXKPHN4QnoVsivZC11GBuKEWwLAkgmGE3MhJuryCqHgqiArIUzsLnYZA61T2JQ6LBtVUY74ffgc7u2rcWbRDyDryruKjsotKHWM020OTbS9qdJxcuC81DtnqEbAo3ibSTJ7IFTT1wmQZDZD',
+    FB_VERIFY_TOKEN: 'uwitestbot',
+    API_AI_CLIENT_ACCESS_TOKEN: 'c7179fe6758a408f9e0ea70437848f76',
+    FB_APP_SECRET: 'dde725c32e22cdb16327b0ace2f688c4',
+    SERVER_URL: "https://uwicavehillchatbot.herokuapp.com/",
 };
