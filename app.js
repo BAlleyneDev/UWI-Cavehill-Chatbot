be35239df3603d7fa1ee72e41050daeae7f5b19d
Brandon Alleyne <b.alleyne.sa@gmail.com>
2017-09-24 00:27:51 -0400

    Testing V2

--- app.js
+++ app.js
@@ -187,6 +187,7 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
 		
 		case "faq-ques":
 		console.log('BEFORE IF');
+		console.log('Context NAME: '+contexts[0].name);
 		if(isDefined(contexts[0]) && contexts[0].name == 'DefaultFallbackIntent' && contexts[0].parameters)
 		{
 			console.log('After IF');
