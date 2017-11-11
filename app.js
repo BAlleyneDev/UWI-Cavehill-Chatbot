357e58d90e9c2e769bbf6bf38cbe9b84ee2369a9
Brandon Alleyne <b.alleyne.sa@gmail.com>
2017-11-11 15:53:32 -0400

    Welcome user greeting updated

--- app.js
+++ app.js
@@ -707,8 +707,8 @@ function greetUserText(userId) {
 				console.log("FB user: %s %s, %s",
 					user.first_name, user.last_name, user.gender);
 
-				sendTextMessage(userId, "Hello " + user.first_name + ':) Welcome to the UWI Cavehill Bot. I can answer questions about the university that you might have.' 
-				+'What can I help you with?');
+				sendTextMessage(userId, "Hello " + user.first_name + ' :). Welcome to the UWI Cavehill Bot. I can answer questions such as\n What time does CMP Building close?\n How do I log into elearning?\n' 
+				+' What can I help you with?');
 			} else {
 				console.log("Cannot get data for fb user with id",
 					userId);
