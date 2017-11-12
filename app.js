cede2a2f04432f30f9dd8c94c42876f06fc58d61
Brandon Alleyne <b.alleyne.sa@gmail.com>
2017-11-11 22:39:43 -0400

    Adding Database

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const express = require('express');
 const crypto = require('crypto');
 const bodyParser = require('body-parser');
 const request = require('request');
+const pg = require('pg');
 const app = express();
 const uuid = require('uuid');
 
+pg.defaults.ssl = true;
 
 // Messenger API parameters
 if (!config.FB_PAGE_TOKEN) {
@@ -25,6 +27,9 @@ if (!config.FB_APP_SECRET) {
 }
 if (!config.SERVER_URL) { //used for ink to static files
 	throw new Error('missing SERVER_URL');
+}
+if (!config.PG_CONFIG) { //postgresql config obj
+	throw new Error('missing PG_CONFIG');
 }
 
 
@@ -745,6 +750,46 @@ function greetUserText(userId) {
 			var user = JSON.parse(body);
 
 			if (user.first_name) {
+
+
+					var pool = new pg.Pool(config.PG_CONFIG);
+					pool.connect(function(err, client, done) {
+						if (err) {
+							return console.error('Error acquiring client', err.stack);
+						}
+						var rows = [];
+						console.log('fetching user');
+						client.query(`SELECT id FROM users WHERE fb_id='${userId}' LIMIT 1`,
+							function(err, result) {
+								console.log('query result ' + result);
+								if (err) {
+									console.log('Query error: ' + err);
+								} else {
+									console.log('rows: ' + result.rows.length);
+									if (result.rows.length === 0) {
+										let sql = 'INSERT INTO users (fb_id, first_name, last_name, profile_pic, ' +
+											'locale, timezone, gender) VALUES ($1, $2, $3, $4, $5, $6, $7)';
+										console.log('sql: ' + sql);
+										client.query(sql,
+											[
+												userId,
+												user.first_name,
+												user.last_name,
+												user.profile_pic,
+												user.locale,
+												user.timezone,
+												user.gender
+											]);
+									}
+								}
+							});
+
+					});
+					pool.end();
+
+
+
+
 				console.log("FB user: %s %s, %s",
 					user.first_name, user.last_name, user.gender);
 
--- config.js
+++ config.js
@@ -5,5 +5,14 @@ module.exports = {
     FB_APP_SECRET: 'dde725c32e22cdb16327b0ace2f688c4',
     SERVER_URL: "https://uwicavehillchatbot.herokuapp.com/",
     EMAIL_FROM:'b.alleyne.sa@gmail.com',
-    EMAIL_TO:'b.alleyne.sa@gmail.com'
+    EMAIL_TO:'b.alleyne.sa@gmail.com',
+    PG_CONFIG: {
+	user: 'sutnvslnmunngq',
+	database: 'dfjt9cihop67n6',
+	password: 'b42639321db55ac5b31bf5afde5d5e620936fa8df6976fde07bb48860ac9d04d',
+	host: 'ec2-23-21-85-76.compute-1.amazonaws.com',
+	port: 5432,
+	max: 10,
+	idleTimeoutMillis: 30000
+    }
 };
