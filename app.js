b9560ff9a721337f0da650df19526bffb19572ab
Brandon Alleyne <b.alleyne.sa@gmail.com>
2017-11-12 23:12:44 -0400

    query bugs

--- app.js
+++ app.js
@@ -265,7 +265,8 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
 				  var rows = [];
 				  var query = client.query(`SELECT course FROM compsci_courses`,
 				  function(err, result) {
-                        let reply = `The courses available for computer science are ${result.rows}. What is your degree?`;
+					    var value = JSON.stringify(result.rows);
+                        let reply = `The courses available for computer science are ${value}. What is your degree?`;
 					  sendTextMessage(sender, reply);
 					  console.log('reply:'+reply);
 				  }
