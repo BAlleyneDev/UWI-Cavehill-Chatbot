eae4317c746b8926727b1c055795ccba1081958b
Brandon Alleyne <b.alleyne.sa@gmail.com>
2018-04-13 17:05:32 -0400

    Add Email

--- user.js
+++ user.js
@@ -17,7 +17,7 @@ module.exports = function(callback, userId){
 		if (!error && response.statusCode == 200) {
 
 			var user = JSON.parse(body);
-            console.log('USERObject'+user);
+            console.log('USERObject'+user.email);
 			if (user.first_name) {
 
                     console.log("USER FIRST NAME OBTAINED");
