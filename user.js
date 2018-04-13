fcedb411663de1a604cf4ebe318a6c92965410b3
Brandon Alleyne <b.alleyne.sa@gmail.com>
2018-04-13 16:47:06 -0400

    Add Email

--- user.js
+++ user.js
@@ -37,7 +37,7 @@ module.exports = function(callback, userId){
 									console.log('rows: ' + result.rows.length);
 									if (result.rows.length === 0) {
 										let sql = 'INSERT INTO users (fb_id, first_name, last_name, profile_pic, ' +
-											'locale, timezone, gender) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)';
+											'locale, timezone, gender, email) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)';
 										console.log('sql: ' + sql);
 										client.query(sql,
 											[
