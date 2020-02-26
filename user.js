02087ea486324536945c4a0932f514a69b47ded1
BAlleyneDev <tushawrt_ba@hotmail.com>
2020-02-26 01:45:30 -0500

    Update spacing

--- user.js
+++ user.js
@@ -16,56 +16,56 @@ module.exports = function(callback, userId){
 	}, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
 
-			var user = JSON.parse(body);
-            console.log('USERObject'+user.email);
-			if (user.first_name) {
+		var user = JSON.parse(body);
+                console.log('USERObject'+user.email);
+			
+		if (user.first_name) 
+		{
 
-                    console.log("USER FIRST NAME OBTAINED");
-					var pool = new pg.Pool(config.PG_CONFIG);
-					pool.connect(function(err, client, done) {
-						if (err) {
-							return console.error('Error acquiring client', err.stack);
-						}
-						var rows = [];
-						console.log('fetching user');
-						client.query(`SELECT id FROM users WHERE fb_id='${userId}' LIMIT 1`,
-							function(err, result) {
-								console.log('query result ' + result);
-								if (err) {
-									console.log('Query error: ' + err);
-								} else {
-									console.log('rows: ' + result.rows.length);
-									if (result.rows.length === 0) {
-										let sql = 'INSERT INTO users (fb_id, first_name, last_name, profile_pic, ' +
-											'locale, timezone, gender) VALUES ($1, $2, $3, $4, $5, $6, $7)';
-										console.log('sql: ' + sql);
-										client.query(sql,
-											[
-												userId,
-												user.first_name,
-												user.last_name,
-												user.profile_pic,
-												user.locale,
-												user.timezone,
-												user.gender
-											]);
-									}
-									console.log(user.email);
-                                    console.log("SUCCESSFULLY PASSED QUERY")
-								}
-                                console.log(user);
-                                callback(user);
-							});
-
-					});
-					pool.end();
-
+		     console.log("USER FIRST NAME OBTAINED");
+		     var pool = new pg.Pool(config.PG_CONFIG);
+		     pool.connect(function(err, client, done) {
+		     if (err) {
+			return console.error('Error acquiring client', err.stack);
+		      }
+		     var rows = [];
+		     console.log('fetching user');
+		     client.query(`SELECT id FROM users WHERE fb_id='${userId}' LIMIT 1`,
+		     function(err, result) {
+			console.log('query result ' + result);
+			if (err) {
+			    console.log('Query error: ' + err);
 			} else {
-				console.log("Cannot get data for fb user with id",
-					userId);
+			    console.log('rows: ' + result.rows.length);
+			    if (result.rows.length === 0) {
+			    let sql = 'INSERT INTO users (fb_id, first_name, last_name, profile_pic, ' +
+						          'locale, timezone, gender) VALUES ($1, $2, $3, $4, $5, $6, $7)';
+			    console.log('sql: ' + sql);
+			    client.query(sql,
+					[
+					userId,
+					user.first_name,
+					user.last_name,
+					user.profile_pic,
+					user.locale,
+					user.timezone,
+					user.gender
+				        ]);
+				}
+				
+		console.log(user.email);
+	        console.log("SUCCESSFULLY PASSED QUERY")
+		}
+	        console.log(user);
+	        callback(user);
+	        });
+		});
+		pool.end();
+		} else {
+		   console.log("Cannot get data for fb user with id", userId);
 			}
 		} else {
-			console.error(response.error);
+		   console.error(response.error);
 		}
 	});
 }
